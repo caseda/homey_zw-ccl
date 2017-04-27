d775e978f6f355d48bd578bb16e00347a32ba21a
Remco <caseda08@hotmail.com>
2017-04-27 12:24:59 +0200

    humanify PR text for wakeup

--- Wake Up.js
+++ Wake Up.js
@@ -17,7 +17,7 @@
  * To get a certain device's default wake up interval we can use the
  * "Send raw data" in the z-wave settings.
  *
- * NODE_ID = the node id you want the information from (Decimal or Hexadecimal)
+ * NODE_ID = The NodeID you want the information from (Decimal or Hexadecimal)
  * 0x84 = COMMAND_CLASS_WAKE_UP
  * 0x05 = WAKE_UP_INTERVAL_GET
  *
@@ -25,14 +25,13 @@
  *
  * Report back will be in Hexadecimal:
  * COMMAND_CLASS_WAKE_UP, data: 0x06#1#2#3#4
- * 06       = WAKE_UP_INTERVAL_REPORT
- * #1 - #3  = Wake up interval of a device in seconds
- * #4       = NodeID of the device receiving the Wake Up Notification Command (Homey)
+ * 0x06    = WAKE_UP_INTERVAL_REPORT
+ * #1 - #3 = Current Wake up interval (seconds)
+ * #4      = NodeID receiving the "WAKE_UP_NOTIFICATION" (Homey)
  *
- * IE: 0x06000e1001 = 
- * 0x06           = WAKE_UP_INTERVAL_REPORT
- * 000e10 = 3600  = wake up interval of a device
- * 01     = 1     = NodeID of the device receiving the Wake Up Notification Command
+ * IE: data: 0x06000e1001
+ * 000e10 = 3600 = Current Wake up interval (seconds)
+ * 01     = 1    = NodeID receiving the "WAKE_UP_NOTIFICATION" (Homey)
  */
 
 NODE_ID,0x84,0x05
@@ -42,7 +41,7 @@ NODE_ID,0x84,0x05
  * To get a certain device's wake up range we can use the
  * "Send raw data" in the z-wave settings.
  *
- * NODE_ID = the node id you want the information from (Decimal or Hexadecimal)
+ * NODE_ID = The NodeID you want the information from (Decimal or Hexadecimal)
  * 0x84 = COMMAND_CLASS_WAKE_UP
  * 0x09 = WAKE_UP_INTERVAL_CAPABILITIES_GET
  *
@@ -50,18 +49,17 @@ NODE_ID,0x84,0x05
  *
  * Report back will be in Hexadecimal:
  * COMMAND_CLASS_WAKE_UP, data: 0x0a#1#2#3#4#5#6#7#8#9#A#B#C
- * 0a       = WAKE_UP_IntervalCapabilitiesReport
- * #1 - #3  = Minimum Wake Up Interval Seconds
- * #4 - #6  = Maximum Wake Up Interval Seconds
- * #7 - #9  = Default Wake Up Interval Seconds
- * #A - #C  = Wake Up Interval Step Seconds
+ * 0x0a    = WAKE_UP_INTERVAL_CAPABILITIES_REPORT
+ * #1 - #3 = Minimum wake up interval (seconds)
+ * #4 - #6 = Maximum wake up interval (seconds)
+ * #7 - #9 = Default wake up interval (seconds)
+ * #A - #C = Wake up interval step (seconds)
  *
- * IE:  0x0a0000f0000e10000e1000003c = 
- * 0x0a			      = WAKE_UP_IntervalCapabilitiesReport
- * 0000f0 = 240		= Minimum Wake Up Interval Seconds
- * 000e10 = 3600	= Maximum Wake Up Interval Seconds
- * 000e10 = 3600  = Default Wake Up Interval Seconds
- * 00003c = 60		= Wake Up Interval Step Seconds
+ * IE: data: 0x0a0000f0000e10000e1000003c
+ * 0000f0 = 240s  = Minimum wake up interval (seconds)
+ * 000e10 = 3600s = Maximum wake up interval (seconds)
+ * 000e10 = 3600s = Default wake up interval (seconds)
+ * 00003c = 60s   = Wake up interval step (seconds)
  */
 
 NODE_ID,0x84,0x09
